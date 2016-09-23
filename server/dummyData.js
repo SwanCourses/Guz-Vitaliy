a049302950ff8967171cdc6f2a8b8f77e8be9777
Vitaliy <vitaliy.guz@gmail.com>
2016-09-23 14:55:42 +0300

    Home task unit 1 (#1)
    * Home task unit 1
    
    * Home task unit 1

--- server/dummyData.js
+++ server/dummyData.js
@@ -34,12 +34,24 @@ export default function () {
       qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
       ipsum quia dolor sit amet.`;
 
-    const post1 = new Post({ name: 'Admin', title: 'Hello MERN', slug: 'hello-mern', cuid: 'cikqgkv4q01ck7453ualdn3hd', content: content1 });
-    const post2 = new Post({ name: 'Admin', title: 'Lorem Ipsum', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3hf', content: content2 });
+    const content3 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.`;
 
-    Post.create([post1, post2], (error) => {
+    const post1 = new Post({ name: 'Admin', title: 'Hello MERN', slug: 'hello-mern', cuid: 'cikqgkv4q01ck7453ualdn3hd', content: content1 });
+    const post2 = new Post({ name: 'Admin', title: 'Lorem Ipsum', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3hf', content: content3 });
+    const post3 = new Post({ name: 'Admin', title: 'Lorem Ipsum 2', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3h3', content: content2 });
+    const post4 = new Post({ name: 'Admin', title: 'Lorem Ipsum 3', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3h4', content: content3 });
+    const post5 = new Post({ name: 'Admin', title: 'Lorem Ipsum 4', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3h5', content: content2 });
+
+
+    Post.create([post1, post2, post3, post4, post5], (error) => {
       if (!error) {
-        // console.log('ready to go....');
+         console.log('ready to go....');
       }
     });
   });
